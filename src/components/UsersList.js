63ed5ba69bb79f558fab11190a9171d272c19d63
mwoolf87 <mwoolfdev@gmail.com>
2023-04-17 15:52:07 -0400

    created UsersListItem component among other things

--- src/components/UsersList.js
+++ src/components/UsersList.js
@@ -4,6 +4,7 @@ import { useSelector } from "react-redux";
 import { fetchUsers, addUser } from "../store";
 import Button from "./Button";
 import Skeleton from "./Skeleton";
+import UsersListItem from "./UsersListItem";
 
 const UsersList = () => {
   const [doFetchUsers, isLoadingUsers, loadingUsersError] =
@@ -28,13 +29,7 @@ const UsersList = () => {
     content = <div>Error fetching data...</div>;
   } else {
     content = data.map(user => {
-      return (
-        <div key={user.id} className="mb-2 border rounded">
-          <div className="flex p-2 justify-between items-center cursor-pointer">
-            {user.name}
-          </div>
-        </div>
-      );
+      return <UsersListItem key={user.id} user={user} />;
     });
   }
 
