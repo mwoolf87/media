690c758187c63a7e4523047f3b402c4e872fda7d
mwoolf87 <mwoolfdev@gmail.com>
2023-04-17 15:01:40 -0400

    refactored button and userslist components

--- src/components/UsersList.js
+++ src/components/UsersList.js
@@ -17,44 +17,37 @@ const UsersList = () => {
     doFetchUsers();
   }, [doFetchUsers]);
 
-  if (isLoadingUsers) {
-    return (
-      <div>
-        <Skeleton times={4} clasName="h-10 w-full" />
-      </div>
-    );
-  }
-
   const handleUserAdd = () => {
     doCreateUser();
   };
 
-  if (loadingUsersError) {
-    return <div>Error fetching data...</div>;
-  }
-
-  const renderedUsers = data.map(user => {
-    return (
-      <div key={user.id} className="mb-2 border rounded">
-        <div className="flex p-2 justify-between items-center cursor-pointer">
-          {user.name}
+  let content;
+  if (isLoadingUsers) {
+    content = <Skeleton times={6} clasName="h-10 w-full" />;
+  } else if (loadingUsersError) {
+    content = <div>Error fetching data...</div>;
+  } else {
+    content = data.map(user => {
+      return (
+        <div key={user.id} className="mb-2 border rounded">
+          <div className="flex p-2 justify-between items-center cursor-pointer">
+            {user.name}
+          </div>
         </div>
-      </div>
-    );
-  });
+      );
+    });
+  }
 
   return (
     <div>
-      <div className="flex flex-row justify-between m-3">
+      <div className="flex flex-row justify-between items-center m-3">
         <h1 className="m-2 text-xl">Users</h1>
-        {isCreatingUser ? (
-          "Creating User..."
-        ) : (
-          <Button onClick={handleUserAdd}>+ Add User</Button>
-        )}
+        <Button loading={isCreatingUser} onClick={handleUserAdd}>
+          + Add User
+        </Button>
         {creatingUserError && "Error creating user..."}
       </div>
-      {renderedUsers}
+      {content}
     </div>
   );
 };
