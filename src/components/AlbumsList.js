183eed297b5dcfa3342802e3d3581e01e11ab6ba
mwoolf87 <mwoolfdev@gmail.com>
2023-04-20 22:23:57 -0400

    refactored RTK tags to better fetch and re-render data

--- src/components/AlbumsList.js
+++ src/components/AlbumsList.js
@@ -1,11 +1,10 @@
 import React from "react";
 import { useFetchAlbumsQuery, useAddAlbumMutation } from "../store";
 import Skeleton from "./Skeleton";
-import ExpandablePanel from "./ExpandablePanel";
 import Button from "./Button";
 import AlbumsListItem from "./AlbumsListItem";
 const AlbumsList = ({ user }) => {
-  const { data, error, isLoading } = useFetchAlbumsQuery(user);
+  const { data, error, isFetching } = useFetchAlbumsQuery(user);
   const [addAlbum, results] = useAddAlbumMutation();
 
   const handleAddAlbum = () => {
@@ -13,7 +12,7 @@ const AlbumsList = ({ user }) => {
   };
 
   let content;
-  if (isLoading) {
+  if (isFetching) {
     content = <Skeleton clasName="h-10 w-full" times={3} />;
   } else if (error) {
     content = <div>Error loading ablums.</div>;
